dd35a53684e538c69acfd5a59556e9dbce79a9be
Joyce Tao <jtao@lcs.on.ca>
2021-11-24 11:32:01 -0500

    Initial Commit

---
+++ MoodDiary/MoodDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodDiaryApp.swift
+//  MoodDiary
+//
+//  Created by Joyce Tao on 2021-11-24.
+//
+
+import SwiftUI
+
+@main
+struct MoodDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
