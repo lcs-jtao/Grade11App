2523f3fc54e78a0256c257d570ba5759736a180d
Joyce Tao <jtao@lcs.on.ca>
2021-12-06 12:29:26 -0500

    Made a static version of the main interface

--- MoodDiary/Views/ContentView.swift
+++ MoodDiary/Views/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    var date = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            Text("2021")
+                .font(.title)
+                .bold()
+            Text("November")
+                .bold()
+            Spacer()
+            ForEach([0, 5, 10, 15, 20, 25], id: \.self) { r in
+                HStack {
+                    ForEach(1...5, id: \.self) { i in
+                        ZStack {
+                            Circle()
+                                .fill(Color.blue.opacity(0.3))
+                                .frame(width: 50, height: 50)
+                            Text("\(i + r)")
+                        }
+                    }
+                }
+                Spacer()
+            }
+        }
     }
 }
 
