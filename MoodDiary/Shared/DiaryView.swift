35aff950dad67d0845b208f746c34e5483dede31
Joyce Tao <jtao@lcs.on.ca>
2022-02-16 12:25:49 -0500

    Created the basic interface for DiaryView (mood image + text field)

---
+++ MoodDiary/Shared/DiaryView.swift
@@ -0,0 +1,45 @@
+//
+//  DiaryView.swift
+//  MoodDiary
+//
+//  Created by Joyce Tao on 2022-02-16.
+//
+
+import SwiftUI
+
+struct DiaryView: View {
+    
+    @State var inputText = ""
+    @State var mood: String
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                Rectangle()
+                    .strokeBorder()
+                    .frame(width: 300, height: 200)
+                VStack {
+                    Image(mood)
+                        .resizable()
+                        .frame(width: 80, height: 80)
+                    Text(mood)
+                        .font(.title)
+                        .bold()
+                        .shadow(color: Color.green, radius: 4, x: 0, y: 6)
+                        .foregroundColor(Color.black.opacity(0.6))
+                    TextField("Tell us about your day!", text: $inputText)
+                }
+            }
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct DiaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DiaryView(mood: "Happy")
+        }
+    }
+}
--- MoodDiary/Shared/EmotionButton.swift
+++ MoodDiary/Shared/EmotionButton.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 struct EmotionButton: View {
     let imageName: String
+    @State var chosenMood = ""
     var body: some View {
-        NavigationLink(destination: DiaryView(), label: {
+        NavigationLink(destination: DiaryView(mood: chosenMood), label: {
             Image(imageName)
                 .resizable()
                 .frame(width: 70, height: 70)
         })
+        .task {
+            chosenMood = imageName
+        }
     }
 }
